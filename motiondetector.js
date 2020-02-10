136477bc865139a364922fe7d1c35a3e178999be
Pamp <pamp@Pamps-MacBook-Pro.local>
2020-02-10 22:50:34 +0000

    good wee bits

--- motiondetector.js
+++ motiondetector.js
@@ -1,13 +1,3 @@
-/*********************************************************************
-*  #### JS Motion Visualiser ####
-*  Coded by Jason Mayes. www.jasonmayes.com
-*  Please keep this disclaimer with my code if you use it anywhere. 
-*  Thanks. :-)
-*  Got feedback or questions, ask here:
-*  Github: https://github.com/jasonmayes/JS-Motion-Detection/
-*  Updates will be posted to this site.
-*********************************************************************/
-
 // Cross browser support to fetch the correct getUserMedia object.
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
   || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -16,20 +6,24 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
 window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
 
-var MotionDetector = (function() {
-  var alpha = 0.5;
+var MotionCapture = (function() {
   var version = 0;
-  var greyScale = false;
+  var stepCount = 0;
 
   var canvas = document.getElementById('canvas');
-  var canvasFinal = document.getElementById('canvasFinal');
   var video = document.getElementById('camStream');
   var ctx = canvas.getContext('2d');
-  var ctxFinal = canvasFinal.getContext('2d');
+  var animationCanvas = document.getElementById('animation');
+  var aniCtx = animationCanvas.getContext('2d');
+  var threshold = 830000;
   var localStream = null;
   var imgData = null;
-  var imgDataPrev = [];
+  var imgDataPrev = [null,null];
+  var frames = [];
+  var frame = -1;
+  var playing = true;
 
+  animationCanvas.onclick = function(){playing=!playing;}
  
   function success(stream) {
     localStream = stream;
@@ -48,73 +42,78 @@ var MotionDetector = (function() {
     if (localStream) {
       canvas.width = video.offsetWidth;
       canvas.height = video.offsetHeight;
-      canvasFinal.width = video.offsetWidth;
-      canvasFinal.height = video.offsetHeight;
-
+      animationCanvas.width = video.offsetWidth;
+      animationCanvas.height = video.offsetHeight;
+  
+      threshold = document.getElementById('threshold').value;
       ctx.drawImage(video, 0, 0);
 
       // Must capture image data in new instance as it is a live reference.
       // Use alternative live referneces to prevent messed up data.
       imgDataPrev[version] = ctx.getImageData(0, 0, canvas.width, canvas.height);
       version = (version == 0) ? 1 : 0;
-
       imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
       var length = imgData.data.length;
-      var x = 0;
-      while (x < length) {
-        if (!greyScale) {
-          // Alpha blending formula: out = (alpha * new) + (1 - alpha) * old.
-          imgData.data[x] = alpha * (255 - imgData.data[x]) + ((1-alpha) * imgDataPrev[version].data[x]);
-          imgData.data[x + 1] = alpha * (255 - imgData.data[x+1]) + ((1-alpha) * imgDataPrev[version].data[x + 1]);
-          imgData.data[x + 2] = alpha * (255 - imgData.data[x+2]) + ((1-alpha) * imgDataPrev[version].data[x + 2]);
-          imgData.data[x + 3] = 255;
-        } else {
-          // GreyScale.
-          var av = (imgData.data[x] + imgData.data[x + 1] + imgData.data[x + 2]) / 3;
-          var av2 = (imgDataPrev[version].data[x] + imgDataPrev[version].data[x + 1] + imgDataPrev[version].data[x + 2]) / 3;
-          var blended = alpha * (255 - av) + ((1-alpha) * av2);
-          imgData.data[x] = blended;
-          imgData.data[x + 1] = blended;
-          imgData.data[x + 2] = blended;
-          imgData.data[x + 3] = 255;
-        }
-        x += 4; 
-      }
-      ctxFinal.putImageData(imgData, 0, 0);
-      console.log(compareTwoImages(imgDataPrev[0],imgDataPrev[1]));
+      document.getElementById('threshold').max=length;
 
-      if(compareTwoImages(imgDataPrev[0],imgDataPrev[1]) > 700000)
+      var difference = compareTwoImages(imgDataPrev[0],imgDataPrev[1]);
+      if(difference > threshold)
       {
         captureImage(canvas);
       }
-
-
+      if(difference>0){
+        document.getElementById('motion').innerHTML = (compareTwoImages(imgDataPrev[0],imgDataPrev[1])) + ' <strong>/</strong> ' + length;
+      }
     }
-
   }
 
-  function captureImage(ctx){
+function captureImage(ctx){
+  frames.push(imgData);
+
 var image = document.createElement('img');
+var dataUrl = ctx.toDataURL();
 image.style.width='20px';
-image.src = ctx.toDataURL();
-document.getElementById('captures').append(image);
+image.src = dataUrl;
 image.onclick = function(){
-  [].slice.call(document.getElementById('captures').getElementsByTagName('img')).forEach(function(x,i){
-    x.style.width='20px';
+  var img = new Image();
+  img.src=this.src;
+  playing = false;
+  [].slice.call(document.getElementById('captures').getElementsByTagName('img'))
+  .forEach(function(x,i){
+    x.style.opacity=1;
   })
-  this.style.width = '500px';
+  aniCtx.drawImage(img,0,0);
+  this.style.opacity=0.5;
 }
-  }
+document.getElementById('captures').append(image);
+
+}
 
 function compareTwoImages(img1,img2){
   var diffPixels = 0;
+  if(img1!==null && img2!==null){
 for(var i =0; i < img1.data.length; i++){
   if(img1.data[i] !=img2.data[i]){
     diffPixels++;
   }
 }
+  }
 return diffPixels;
+}
+
+function step(){
+  stepCount++;
+  if(stepCount%30 && playing){
+    snapshot();
+  if(frame<frames.length){
+    frame++;
+  } else{frame=0;}
+  if(frames[frame]!==undefined){
+aniCtx.putImageData(frames[frame],0,0);
+  }
+}
+window.requestAnimationFrame(step);
 }
 
   
@@ -124,7 +123,8 @@ return diffPixels;
     } else { 
       console.error('Your browser does not support getUserMedia');
     }
-    window.setInterval(snapshot, 32);
+    // window.setInterval(snapshot, 32);
+    window.requestAnimationFrame(step);
   }
 
   return {
@@ -132,4 +132,4 @@ return diffPixels;
   };
 })();
 
-MotionDetector.init();
+MotionCapture.init();
